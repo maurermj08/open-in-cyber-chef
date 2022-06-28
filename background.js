569c06eb301d927c6b3c9b2646bde0dc3aa8bec8
wesinator <5124946+wesinator@users.noreply.github.com>
2022-06-27 21:47:57 -0400

    migrate extension to manifest v3

---
+++ background.js
@@ -0,0 +1,20 @@
+openInCyberChef = function(word){
+  var query = word.selectionText;
+  chrome.storage.sync.get("my_url", function(items) {
+    if (!!items.my_url) {
+    chrome.tabs.create({url: items.my_url + "#input=" + encodeURIComponent(btoa(query))});
+    } else {
+    chrome.tabs.create({url: "https://gchq.github.io/CyberChef#input=" + encodeURIComponent(btoa(query))});
+    }
+  });
+};
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: "cyberchef-link",
+        title: "Open in CyberChef",
+        contexts: ["selection"]
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(openInCyberChef);
